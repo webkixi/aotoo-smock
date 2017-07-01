3fa60772be0ba3e82bdcff7b5f33a3efb2c47ce5
yc <kixi@163.com>
2017-07-01 17:09:25 +0800

    优化itemclass

--- src/index.js
+++ src/index.js
@@ -67,10 +67,16 @@ function prepaireData(state){
    * ]
    */
   const that = this
+  const props = this.props
+  const propsItemClass = props.itemClass ? props.itemClass + ' ' : ''
   let menuData = []
   let contentData = []
   state.data.forEach( (item, ii) => {
-    const itemCls = ii == state.select ? item.itemClass ? item.itemClass+' select' : 'select' : ''
+    // console.log(item);
+    const itemCls = ii == state.select 
+      ? item.itemClass ? propsItemClass+item.itemClass+' select' : propsItemClass+'select' 
+      : item.itemClass ? propsItemClass+item.itemClass : propsItemClass
+    
     // 准备菜单数据
     menuData.push({
       index: ii,
@@ -515,11 +521,17 @@ const tabs = Aotoo.tabs({
     tabClass: 'tabs-nornal-top',
     data: [
       // {title: 'aaa', content: '什么', idf: 'le1', itemClass: 'aabbcc'},
-      {title: 'aaa', content: '什么, what', path: 'a1', attr:{path: 'a1'}},
+      {title: 'aaa', content: '什么, what', path: 'a1', attr:{path: 'a1'}, idf: 'aa1'},
+      {title: 'aaa1', content: '什么1', path: 'a-1', attr:{path: 'a1'}, idf: 'aa2', parent: 'aa1'},
+      {title: 'aaa2', content: '什么2', path: 'a-2', attr:{path: 'a1'}, idf: 'aa3', parent: 'aa2'},
+      {title: 'aaa3', content: '什么3', path: 'a-3', attr:{path: 'a1'}, itemClass: 'yyy',             parent: 'aa3'},
+      {title: 'aaa4', content: '什么4', path: 'a-4', attr:{path: 'a1'}, itemClass: 'yyy',             parent: 'aa3'},
       {title: 'bbb', content: '来了', path: 'a2', attr:{path: 'a2'}},
-      {title: 'ccc', content: <WrapElement />, path: 'a3', attr:{path: 'a3'}},
+      {title: 'ccc', content: <WrapElement />, path: 'a3', attr:{path: 'a3'}, itemClass: 'yyy'},
     ],
+    itemClass: 'nihao',
     itemMethod: function(dom){
+      $(dom).find('li.nihao:not()')
       const _path = $(dom).attr('data-path')
       $(dom).click(function(){
         tabs.goto(_path)

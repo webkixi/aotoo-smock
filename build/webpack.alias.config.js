2f509739fc2530f7780a7a124749d8daa621cd61
yc <kixi@163.com>
2018-06-09 11:51:42 +0800

    编译 --- webpack_alias

--- build/webpack.alias.config.js
+++ build/webpack.alias.config.js
@@ -2,5 +2,6 @@ const path = require('path')
 module.exports = {
   root: path.join(__dirname, '../'),
   css: path.join(__dirname, '../src/css'),
+  component: path.join(__dirname, '../component')
 }
 
